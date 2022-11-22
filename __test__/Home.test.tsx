73ac811099fa55a72dfca24fed59682b2c30bb2f
massu-159 <magjtwjp@gmail.com>
2022-11-23 08:56:04 +0900

    fix render test

--- __test__/Home.test.tsx
+++ __test__/Home.test.tsx
@@ -4,6 +4,6 @@ import Home from '../pages/index'
 
 it('Should render hello text', () => {
   render(<Home />)
-  expect(screen.getByText('Hello Nextjs')).toBeInTheDocument()
+  expect(screen.getByText('Welcome to Nextjs')).toBeInTheDocument()
   // screen.debug();
 })
