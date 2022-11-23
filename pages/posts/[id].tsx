24df18e44d5bcf0eba691adcf313c3fa6df3b3b9
massu-159 <magjtwjp@gmail.com>
2022-11-23 20:59:59 +0900

    create postDetail page

---
+++ pages/posts/[id].tsx
@@ -0,0 +1,56 @@
+import { GetStaticPaths, GetStaticProps } from 'next'
+import Link from 'next/link'
+import React from 'react'
+import Layout from '../../components/Layout'
+import { getAllPostIds, getPostData } from '../../lib/fetch'
+import { Post } from '../../types/Types'
+
+const PostDetail = ({ id, title, body }: Post) => {
+  return (
+    <Layout title={title}>
+      <p className="m-4">
+        {'ID : '}
+        {id}
+      </p>
+      <p className="mb-4 text-xl font-bold">{title}</p>
+      <p className="mx-10 mb-12">{body}</p>
+      <Link href="/blogPage">
+        <div className="mt-12 flex cursor-pointer">
+          <svg
+            className="mr-3 h-6 w-6"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M11 19l-7-7 7-7m8 14l-7-7 7-7"
+            />
+          </svg>
+          <a data-testid="back-blog">Back to blogPage</a>
+        </div>
+      </Link>
+    </Layout>
+  )
+}
+export default PostDetail
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const paths = await getAllPostIds()
+  return {
+    paths,
+    fallback: false,
+  }
+}
+
+export const getStaticProps: GetStaticProps = async (ctx) => {
+  const post = await getPostData(ctx.params?.id as string)
+  return {
+    props: {
+      ...post,
+    },
+  }
+}
