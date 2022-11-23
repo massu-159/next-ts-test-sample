fa4c17d17c54af55d16656abf1d2505cd4f43e91
massu-159 <magjtwjp@gmail.com>
2022-11-23 21:59:59 +0900

    update static fetchData

--- pages/blogPage.tsx
+++ pages/blogPage.tsx
@@ -1,14 +1,30 @@
+import { GetStaticProps } from 'next'
 import React from 'react'
 import Layout from '../components/Layout'
+import Post from '../components/Post'
+import { getAllPostsData } from '../lib/fetch'
+import { StaticProps } from '../types/StaticProps'
 
-const BlogPage = () => {
+
+const BlogPage = ({posts}: StaticProps) => {
   return (
     <Layout title={"Blog"}>
       <p className="text-4xl">
         Blog
       </p>
+      <ul>
+        {posts && posts.map((post) => (
+          <Post key={post.id} {...post}></Post>
+        ))}
+      </ul>
     </Layout>
   )
 }
-
 export default BlogPage
+
+export const getStaticProps: GetStaticProps = async () => {
+  const posts = await getAllPostsData()
+  return {
+    props: {posts},
+  }
+}
