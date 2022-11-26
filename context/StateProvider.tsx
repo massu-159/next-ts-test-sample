09860d47f4f8aa22d9ec877484ae46b373ba2a54
massu-159 <magjtwjp@gmail.com>
2022-11-26 10:03:54 +0900

    create global context

---
+++ context/StateProvider.tsx
@@ -0,0 +1,30 @@
+import React, {
+  createContext,
+  Dispatch,
+  SetStateAction,
+  useContext,
+  useState,
+} from 'react'
+import { CommentProps } from '../types/CommentProps'
+
+import Image from 'next/image'
+
+const StateContext = createContext(
+  {} as {
+    toggle: boolean
+    setToggle: Dispatch<SetStateAction<boolean>>
+  }
+)
+
+const StateProvider = ({ children }: CommentProps) => {
+  const [toggle, setToggle] = useState(false)
+  return (
+    <StateContext.Provider value={{ toggle, setToggle }}>
+      {children}
+    </StateContext.Provider>
+  )
+}
+
+export default StateProvider
+
+export const useStateContext = () => useContext(StateContext)
