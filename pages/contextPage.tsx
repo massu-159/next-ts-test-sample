32f1516eaa917646e6b08de88cdb7ff2cb77cd4a
massu-159 <magjtwjp@gmail.com>
2022-11-26 10:04:38 +0900

    update page contexts

--- pages/contextPage.tsx
+++ pages/contextPage.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
+import ContextA from '../components/ContextA'
+import ContextB from '../components/ContextB'
 import Layout from '../components/Layout'
+import StateProvider from '../context/StateProvider'
 
 const ContextPage = () => {
   return (
     <Layout title={'Context'}>
-      <p className="text-4xl">Context</p>
+      <p className="text-4xl mb-10">Context</p>
+      <StateProvider>
+        <ContextA></ContextA>
+        <ContextB></ContextB>
+      </StateProvider>
     </Layout>
   )
 }
