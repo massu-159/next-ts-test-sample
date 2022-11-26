c266b0eb7e04248f71f11d261405014d4fa567ba
massu-159 <magjtwjp@gmail.com>
2022-11-26 10:04:11 +0900

    create context components

---
+++ components/ContextB.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { useStateContext } from '../context/StateProvider'
+
+const ContextB = () => {
+  const { toggle } = useStateContext();
+
+  return (
+    <>
+      <p>Context B</p>
+      <p className="mb-5 text-indigo-600" data-testid="toggle-b">
+        {toggle ? 'true' : 'false'}
+      </p>
+    </>
+  )
+}
+
+export default ContextB
